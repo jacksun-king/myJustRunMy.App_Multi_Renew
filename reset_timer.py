4f6648092b25758d0100ed6465fbc46d9245e489
jacksun-king <jacksun950212@gmail.com>
2026-08-08 09:03:03 +0800

    Update reset_timer.py

--- reset_timer.py
+++ reset_timer.py
@@ -311,7 +311,7 @@ def renew(sb)->bool:
     sb.open("https://justrunmy.app/panel")
     time.sleep(5)
 	print("自动读取应用名称...")
-    retry_count = 3
+	retry_count = 3
     found = False
     
     for attempt in range(1, retry_count + 1):
